871bce099603c1fd37685cbbc743c80d49fbce09
Daniel Sottile <danielsottile@gmail.com>
2020-09-29 19:47:40 -0400

    set up expo sqlite instead because it actually works!

--- Components/App/App.tsx
+++ Components/App/App.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import * as SQLite from 'expo-sqlite';
 import Landing from '../../Routes/Landing/Landing';
 import Load from '../../Routes/Load/Load';
 import Main from '../../Routes/Main/Main';
 
-
-const Stack = createStackNavigator();
-
 const App = () => {
+  /* --- This section creates and opens our DB for use globally in the app ---*/
+
+  const globalAny: any = global;
+
+  globalAny.db = SQLite.openDatabase('DB'); // creates if it does not exist
+
+  console.log(globalAny.db);
+
+  /* --- --- */
+
+  const Stack = createStackNavigator();
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Landing" component={Landing}/>
-        <Stack.Screen name="Load" component={Load}/>
+        <Stack.Screen name="Landing" component={Landing} />
+        <Stack.Screen name="Load" component={Load} />
         <Stack.Screen name="Main" component={Main} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default App;
--- Routes/Landing/Landing.tsx
+++ Routes/Landing/Landing.tsx
@@ -2,26 +2,16 @@ import React from 'react';
 import { Text, View, Image, Button } from 'react-native';
 import LandingStyles from './LandingStyles';
 
-const Landing = ({navigation}: any) => {
+const Landing = ({ navigation }: any) => {
   return (
     <View style={LandingStyles.container}>
-      <Image style={LandingStyles.logo} source={{uri: ''}}/>
+      <Image style={LandingStyles.logo} source={{ uri: '' }} />
       <Text>BrainState</Text>
       <Text>A mind-mapping application for your mobile device!</Text>
       <Text>Make A New Map!</Text>
-      <Button
-      title="+ New Map"
-      onPress={() =>
-        navigation.navigate('Main')
-      }
-      />
+      <Button title="+ New Map" onPress={() => navigation.navigate('Main')} />
       <Text>Load A Previous Map!</Text>
-      <Button
-      title="Load Map"
-      onPress={() =>
-        navigation.navigate('Load')
-      }
-      />
+      <Button title="Load Map" onPress={() => navigation.navigate('Load')} />
     </View>
   );
 };
